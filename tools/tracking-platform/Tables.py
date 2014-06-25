10eb91fd07d694049bf895ea5b09279cad445ffe
Alexey Klimov <alexey.klimov.dev@gmail.com>
2014-06-26 00:45:34 +0300

    Finished filling of the database. Added clean command

--- tools/tracking-platform/Tables.py
+++ tools/tracking-platform/Tables.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     raise SystemExit
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship, backref
 
 _Base = declarative_base()
@@ -24,12 +24,19 @@ launches_diagnostics = Table('launches_diagnostics', _Base.metadata,
     Column('diagnostic_id', Integer, ForeignKey('diagnostics.id'))
 )
 
-class Files(_Base):
+launches_pathpieces = Table('launches_pathpieces', _Base.metadata,
+    Column('launch_id', Integer, ForeignKey('launches.id')),
+    Column('pathpiece_id', Integer, ForeignKey('path_pieces.id'))
+)
+
+class File(_Base):
     __tablename__ = 'files'
 
     id      = Column(Integer, primary_key=True)
     name    = Column(String)
     
+
+
 class Diagnostic(_Base):
     __tablename__ = 'diagnostics'
 
@@ -40,36 +47,49 @@ class Diagnostic(_Base):
     issue_context_kind  = Column(String)
     issue_context       = Column(String) 
     issue_hash          = Column(String, unique=True)
-    path                = relationship( "Path", backref="diagnostic" )
+    
+    class IllegalLaunch( BaseException ):
+        pass
 
-    line                = Column(Integer)
-    column              = Column(Integer)    
-    file_id             = Column(Integer, ForeignKey('files.id'))
-    file                = relationship("files")
-    launches            = relationship("launches", secondary=launches_diagnostics)
+    all_path_pieces     = relationship("PathPiece")
 
-class Path(_Base):
-    __tablename__ = 'paths'
-
-    id                  = Column(Integer, primary_key=True)
-    diagnostic_id       = Column(Integer, ForeignKey('diagnostics.id'))
-    kind                = Column(String)
-    message             = Column(String)
-    extended_message    = Column(String)
-    depth               = Column(Integer)
-    piece_id            = Column(Integer)
+    def getPath( self, _launch ):
+        if _launch not in self.launches:
+            raise IllegalLaunch()
+        proper_piece_ids = object_session(self).select( launches_pathpieces.pathpiece_id ).where( launches_pathpieces.launches == _launch.id ).all()
+        return object_session(self).select( PathPiece ).filter( (PathPiece.diagnostic_id == self.id) and (PathPiece.id in proper_piece_ids) ).all()
 
     line                = Column(Integer)
     col                 = Column(Integer)    
     file_id             = Column(Integer, ForeignKey('files.id'))
-    file                = relationship("files")
+    file                = relationship("File")
+    launches            = relationship("Launch", secondary=launches_diagnostics, backref='diagnostics')
+
+class PathPiece(_Base):
+    __tablename__ = 'path_pieces'
+
+    id                  = Column(Integer, primary_key=True)
+    diagnostic_id       = Column(Integer, ForeignKey('diagnostics.id'))
+    message             = Column(String)
+    extended_message    = Column(String)
+    depth               = Column(Integer)
+    piece_id            = Column(Integer)
+    # TODO: add optional isKeyEvent
+    # TODO: add optional ranges
+    line                = Column(Integer)
+    col                 = Column(Integer)    
+    file_id             = Column(Integer, ForeignKey('files.id'))
+    file                = relationship("File")
+    launches            = relationship("Launch", secondary=launches_pathpieces)
+
+    __table_args__ = ( UniqueConstraint('diagnostic_id', 'message','extended_message','depth','piece_id','line','col','file_id'), )
+
 
 class Launch(_Base):
     __tablename__ = 'launches'
     id                  = Column(Integer, primary_key=True)
     ## Some other information...
     time                = Column(DateTime)
-    diagnostics         = relationship("diagnostics", secondary=launches_diagnostics)
 
  
 ###############################################################################
